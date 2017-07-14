2ae30fae1ea08cdbb1710cc34b4028412ebda73b
GrgurDuj <grga281@gmail.com>
2017-07-14 16:20:16 +0200

    Add files via upload

--- ItemGetter/template_one.py
+++ ItemGetter/template_one.py
@@ -2,11 +2,11 @@ import os
 from datetime import datetime
 from collections import deque
 import time as t
-
+import pickle
 from cassiopeia import riotapi
 from cassiopeia.type.api.exception import APIError
 from cassiopeia.type.core.common import LoadPolicy
-from set_up import set_up
+from set_up import set_up, set_up2, set_up3
 
 set_up('KR')
 
@@ -25,6 +25,9 @@ def auto_retry(api_call_method):
                 except APIError as another_error:
                     if another_error.error_code in [500, 400, 404]:
                         pass
+                    elif another_error.error_code in [503]:
+                        print('Found a 503')
+                        t.sleep(120)
                     else:
                         raise another_error
 
@@ -44,10 +47,7 @@ riotapi.get_summoner_by_name = auto_retry(riotapi.get_summoner_by_name)
 
 unpulled_summoners = deque(entry.summoner for entry in riotapi.get_challenger())
 
-pulled_summoners = deque()
-gather_start = datetime(2017, 7, 9)
-match_set = set()
-
+gather_start = datetime(2017, 7, 12)
 total_req = 0
 
 
@@ -67,34 +67,49 @@ def inc_req(inc):
     total_req += inc
     print('inc print::new', total_req)
 
-i=0
-
+pulled_summoners = deque()
+match_set = set()
+i = 1
 while len(unpulled_summoners) > 0:
-    summoner = unpulled_summoners.popleft()
-    check_req()
-    if i is not 0:
-        t.sleep(120)
-        total_req = 0
-    i+=1
-    summMatchList = summoner.match_list(begin_time=gather_start)
-    inc_req(len(summMatchList))
-    print('summMatchList' , len(summMatchList))
-    for match_reference in summMatchList:
+    try:
+        if i in range(1,201,3):
+            set_up('KR')
+            print('set_up1')
+        elif i in range(2,201,3):
+            set_up2('KR')
+            print('set_up2')
+        elif i in range(3,201,3):
+            set_up3('KR')
+            print('set_up3')
+        summoner = unpulled_summoners.popleft()
+        summMatchList = summoner.match_list(begin_time=gather_start)
+        print('summMatchList' , len(summMatchList))
+        for match_reference in summMatchList:
         # If you are connected to a database, the match will automatically be stored in it.
         # Simply pull the match, and it's in your database for whenever you need it again!
         # If you pull a match twice, the second time it will be loaded from the database rather than pulled from Riot
         # and therefore will not count against your rate limit. This is true of all datatypes, include Summoner.
-        check_req()
-        print('?',total_req)
-        match = riotapi.get_match(match_reference)
-        inc_req(len(match))
-        if match is None:  # If the match still fails to load, continue on to the next one
+            match = riotapi.get_match(match_reference)
+            # t.sleep(15)
+            if match is None:  # If the match still fails to load, continue on to the next one
                 continue
-        elif str(match) in match_set:
-            print('Found duplicate' + match)
-            continue
-        else:
-            match_set.add(match)
-            print("Stored {0} in my database".format(match))
+            elif str(match) in match_set:
+                print('Found duplicate' + match)
+                continue
+            else:
+                match_set.add(match)
+                print("Stored {0} in my database".format(match))
+                #t.sleep(15)
+                with open('match.pickle', 'wb') as handle:
+                    pickle.dump(match, handle, protocol=pickle.HIGHEST_PROTOCOL)
         pulled_summoners.append(summoner)
-    print(len(pulled_summoners))
+        i+=1
+        #print('Sleeping')
+        #t.sleep(60)
+        print(len(pulled_summoners))
+    except:
+        i+=1
+        print('503')
+        t.sleep(30)
+        continue
+print('Done')
